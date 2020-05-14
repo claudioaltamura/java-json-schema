e330aa2ad34c87b8c01901876db06df0f86e6d93
Claudio Altamura <claudio@claudioaltamura.de>
2020-05-14 11:55:21 +0200

    new: added bad test case

--- src/test/java/de/claudioaltamura/java/jsonschema/NetworkNtJsonSchemaValidatorTest.java
+++ src/test/java/de/claudioaltamura/java/jsonschema/NetworkNtJsonSchemaValidatorTest.java
@@ -1,9 +1,6 @@
 package de.claudioaltamura.java.jsonschema;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,24 +11,46 @@ import com.networknt.schema.ValidationMessage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
-import org.everit.json.schema.ValidationException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * @see <a href="https://github.com/networknt/json-schema-validator>networknt json-schema-validator</a>
+ * @see <a href="https://github.com/networknt/json-schema-validator>networknt
+ *     json-schema-validator</a>
  */
 class NetworkNtJsonSchemaValidatorTest {
 
+  private ObjectMapper mapper = new ObjectMapper();
+  private JsonSchema schema;
+
+  @BeforeEach
+  public void beforeAll() {
+    JsonSchemaFactory factory = JsonSchemaFactory.getInstance(VersionFlag.V4);
+    InputStream is =
+        NetworkNtJsonSchemaValidatorTest.class.getResourceAsStream("/product.schema.json");
+    schema = factory.getSchema(is);
+  }
+
+  @Test
+  @DisplayName("invalid input when validating then invalid with details")
+  void givenInvalidInput_whenValidating_thenInvalid_With_Details() throws IOException {
+    JsonNode node =
+        mapper.readTree(
+            NetworkNtJsonSchemaValidatorTest.class.getResourceAsStream("/product-invalid.json"));
+
+    Set<ValidationMessage> errors = schema.validate(node);
+    assertThat(errors.size()).isEqualTo(1);
+    assertThat(errors.iterator().next().getMessage())
+        .contains("price: must have a minimum value of 0");
+  }
+
   @Test
   @DisplayName("valid input when validating then valid")
-  void givenValidInput_whenValidating_thenValid() throws ValidationException, IOException {
-    JsonSchemaFactory factory = JsonSchemaFactory.getInstance(VersionFlag.V4);
-    InputStream is = NetworkNtJsonSchemaValidatorTest.class.getResourceAsStream("/product.schema.json");
-    JsonSchema schema = factory.getSchema(is);
-
-    ObjectMapper mapper = new ObjectMapper();
-    JsonNode node = mapper.readTree(NetworkNtJsonSchemaValidatorTest.class.getResourceAsStream("/product.json"));
+  void givenValidInput_whenValidating_thenValid() throws IOException {
+    JsonNode node =
+        mapper.readTree(
+            NetworkNtJsonSchemaValidatorTest.class.getResourceAsStream("/product.json"));
 
     Set<ValidationMessage> errors = schema.validate(node);
     assertThat(errors.size()).isEqualTo(0);
